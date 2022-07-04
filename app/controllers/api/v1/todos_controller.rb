8b328a0f2182c6c6b318808a5da588f469199686
Alik <alikrugl11@gmail.com>
2022-07-04 09:16:31 +0300

    master: rails + vue js application init

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,62 @@
+module Api
+  module V1
+    class TodosController < ApplicationController
+      before_action :set_todo, only: %i[ show update destroy ]
+
+      # GET /todos
+      def index
+        @todos = Todo.all
+        limit = params[:_limit]
+
+        if limit.present?
+          limit = limit.to_i
+          @todos = @todos.last(:_limit)
+        end
+
+        render json: @todos.reverse
+      end
+
+      # GET /todos/1
+      def show
+        render json: @todo
+      end
+
+      # POST /todos
+      def create
+        @todo = Todo.new(todo_params)
+
+        if @todo.save
+          render json: @todo, status: :created, location: @todo
+        else
+          render json: @todo.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /todos/1
+      def update
+        if @todo.update(todo_params)
+          render json: @todo
+        else
+          render json: @todo.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /todos/1
+      def destroy
+        @todo.destroy
+      end
+
+      private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_todo
+        @todo = Todo.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def todo_params
+        params.require(:todo).permit(:id, :title, :completed, :_limit)
+      end
+    end
+  end
+end
+
